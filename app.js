d022f3da4858338d0b29e8d6515231f64bfac02b
CaitlinStuder <studercaitlin98@gmail.com>
2022-04-26 03:38:58 -0400

    creating div element

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const holesDiv = document.querySelector('.holes')
 const score = document.querySelector('#score')
 const time = document.querySelector('#time-left')
 const startButton = document.querySelector('#start-button')
+
+
+
+for (let i=1; i<=16; i++) {
+    let hole = document.createElement('div')
+    hole.classList.add('hole')
+    holesDiv.appendChild(hole)
+ 
+}
