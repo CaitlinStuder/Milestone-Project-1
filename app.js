7f5994d67ddca4b0b2461a1ba0a63999de84e588
CaitlinStuder <studercaitlin98@gmail.com>
2022-04-26 03:37:42 -0400

    setting const

---
+++ app.js
@@ -0,0 +1,4 @@
+const holesDiv = document.querySelector('.holes')
+const score = document.querySelector('#score')
+const time = document.querySelector('#time-left')
+const startButton = document.querySelector('#start-button')
